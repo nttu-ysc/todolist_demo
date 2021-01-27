241c463e6837d8a1074b5b7e6173399d18e4b0a1
Danny Cheng <a7932677@gmail.com>
2021-01-27 10:46:08 +0800

    + login view & CSS

---
+++ public_html/login.php
@@ -0,0 +1,11 @@
+<?php include('header.php') ?>
+<?php include('data.php') ?>
+<div id="signIn">
+    <form>
+        <h2>Sign In</h2>
+        <input type="text" placeholder="Enter E-mail">
+        <input type="text" placeholder="Enter Password">
+        <button class="btn">Sign In</button>
+    </form>
+</div>
+<?php include('footer.php') ?>
