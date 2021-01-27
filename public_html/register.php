005a49cb88272af1e177d9777982f1488c698a80
Danny Cheng <a7932677@gmail.com>
2021-01-27 11:56:47 +0800

    + register view & CSS

---
+++ public_html/register.php
@@ -0,0 +1,14 @@
+<?php include('header.php') ?>
+<?php include('data.php') ?>
+
+<div id="signUp">
+    <form>
+        <h2>Sign Up</h2>
+        <input type="text" placeholder="Enter User Name">
+        <input type="text" placeholder="Enter E-mail">
+        <input type="text" placeholder="Enter Password">
+        <input type="text" placeholder="Confirm Password">
+        <button class="btn">Sign Up</button>
+    </form>
+</div>
+<?php include('footer.php') ?>
